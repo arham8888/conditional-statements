1e752633a05dc9bc7441c5789dc798639cf2b95b
arham8888 <arhamhasnain428@gmail.com>
2024-12-02 21:43:06 +0500

    Python

---
+++ Untitled-1.py
@@ -0,0 +1,5 @@
+num=int(input("Please enter the number:"))
+if (  num %2==0 ):
+    print("This number is even")
+else:
+    print("This number is odd")
---
+++ activity3.py
@@ -0,0 +1,13 @@
+ 
+num = int(input("Enter a number: "))  
+
+
+if num > 50:  
+    print("The number is greater than 50.")  
+    
+    if num % 2 == 0:  
+        print("The number is even.")  
+    else:  
+        print("The number is odd.")  
+else:  
+    print("The number is not greater than 50.")
